54e29600591027b923d76180ecbf7ec6f0abbe21
joaosmatos <joaovsmatoss@gmail.com>
2025-12-09 01:23:35 -0300

    arquivos estaticos

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
     const bodyparser = require("body-parser")
     const app = express()
     const admin = require("./rotas/admin")
+    const path = require("path")
     //const mongoose = require("mongoose")
 // configurações
     //bdy parser
@@ -13,7 +14,17 @@
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars');
     // mongoose
+    // public
+        app.use(express.static(path.join(__dirname,"public")))
 // rotas
+    app.get('/', (req, res) => {
+        res.send('rota principal')
+    })
+
+    app.get('/posts', (req, res) => {
+        res.send("lista posts")
+    })
+
     app.use('/admin', admin)
 // outros
 const PORT = 8081
