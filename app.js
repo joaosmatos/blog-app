9645f15732bf6cad93d6d598cd25ed4023e8afca
joaosmatos <joaovsmatoss@gmail.com>
2025-12-15 00:37:07 -0300

    middlewares

--- app.js
+++ app.js
@@ -14,8 +14,20 @@
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars');
     // mongoose
+        mongoose.promise = global.promise;
+        mongoose.conect("mongodb://localhost/blogapp").then(() => {
+            console.log("Conectado ao mongo")
+        }).catch((err) => {
+            console.log("Erro ao se conectar: "+err)
+        })
     // public
         app.use(express.static(path.join(__dirname,"public")))
+
+        app.use((req, res, next ) => {
+            console.log("OI EU SOU UM MIDDLEWARE!")
+            next();
+        })
+
 // rotas
     app.get('/', (req, res) => {
         res.send('rota principal')
