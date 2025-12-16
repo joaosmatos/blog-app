7aa7647b22c1da2ccabea18f8efe73064f7ebe79
joaosmatos <joaovsmatoss@gmail.com>
2025-12-16 01:17:14 -0300

    validando formularios

--- rotas/admin.js
+++ rotas/admin.js
@@ -21,16 +21,39 @@ router.get('/categorias/add', (req, res) => {
 })
 
 router.post("/categorias/nova", (req, res) => {
-    const novaCategoria = {
+
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "nome invalido"})
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "slug invalido"})
+    }
+
+    if(req.body.nome.length < 2){
+        erros.push({texto: "nome da categoria muito pequeno"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addcategorias", {erros: erros})
+    }else{
+      const novaCategoria = {
         nome: req.body.nome,
         slug: req.body.slug
     }
 
     new Categoria(novaCategoria).save().then(() => {
-        console.log("Categoria salva com sucesso!")
+        req.flash("sucess_msg", "categoria criada com sucesso")
+        res.redirect("/admin/categorias")
     }).catch((err) => {
+        req.flash("error_msg", "houve um erro ao salvar a categoria, tente novamente!")
         console.log("Erro ao salvar categoria!")
-    })
+    })  
+    }
+
+    
 })
 
 
