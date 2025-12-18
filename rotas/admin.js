9df15aa29b65eea80e82f4678f9454605837263f
joaosmatos <joaovsmatoss@gmail.com>
2025-12-18 01:58:27 -0300

    listando categorias

--- rotas/admin.js
+++ rotas/admin.js
@@ -13,7 +13,12 @@ router.get('/posts', (req, res) => {
 })
 
 router.get("/categorias", (req, res) => {
-    res.render("adimin/categorias")
+    Categoria.find().sort({date:'desc'}).then((categorias) => {
+        res.render("adimin/categorias", {categorias: categorias})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao listar as categorias")
+        res.redirect("/admin")
+    })
 })
 
 router.get('/categorias/add', (req, res) => {
