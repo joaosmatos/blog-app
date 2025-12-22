9b07717a8d618b2a8884a29e7f4df003de5adaf2
joaosmatos <joaovsmatoss@gmail.com>
2025-12-22 01:11:21 -0300

    formulario de postagens

--- rotas/admin.js
+++ rotas/admin.js
@@ -100,5 +100,19 @@ router.post("/categorias/deletar", (req, res) => {
     })
 })
 
+router.get("/postagens", (req, res) => {
+    res.render("admin/postagens")
+})
+
+router.get("/postagens/add", (req, res) => {
+    Categoria.find().then((categorias) => {
+       res.render("admin/addpostagem", {categorias: categorias}) 
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar o formulario")
+        res.redirect("/admin")
+    })
+    
+})
+
 
 module.exports = router
