bc71918b5b395bec75abbf566c694ea43da0ae74
joaosmatos <joaovsmatoss@gmail.com>
2025-12-23 00:26:17 -0300

    salvando postagens no banco de dados

--- rotas/admin.js
+++ rotas/admin.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require("mongoose")
 require("../models/categoria")
 const categoria = mongoose.model("categorias")
+require('../models/postagem')
+const Postagem = mongoose.model("postagens")
 
 router.get('/',(req, res) => {
     res.render("admin/index")
@@ -112,6 +114,34 @@ router.get("/postagens/add", (req, res) => {
         res.redirect("/admin")
     })
     
+})
+
+router.post("/postagens/nova", (req, res) => {
+    var erros = []
+
+    if(req.body.categoria == "0"){
+        erros.push({texto: "categoria invalida, registre uma categoria"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addpostagem", {erros: erros})
+    }else{
+        const novaPostagem = {
+            titulo: req.body.titulo,
+            descricao: req.body.descricao,
+            conteudo: req.body.conteudo,
+            categoria: req.body.categora,
+            slug: req.body.slug
+        }
+
+        new Postagem(novaPostagem).save().then(() => {
+            req.flash("success_msg", "postagem criada com sucesso!")
+            req.redirect("admin/postagens")
+        }).catch((err) =>{
+            req.flash("error_msg", "Houve um erro durante o salvamento da postagem")
+            res.redirect("admin/postagens")
+        })
+    }
 })
 
 
