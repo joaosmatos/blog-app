1299c6091c6035a6ddedc4cc52db6fcf4c11db88
joaosmatos <joaovsmatoss@gmail.com>
2025-12-27 01:08:46 -0300

    deletando postagens

--- rotas/admin.js
+++ rotas/admin.js
@@ -193,5 +193,15 @@ router.post("/postagem/edit", (req, res) => {
     }))
 })
 
+router.get("/postagens/deletar/:id", (req, res) => {
+    Postagem.remove({_id: req.params.id}).then(() => {
+        req.flash("success_msg", "postagem deletada com sucesso!")
+        res.redirect("/admin/postagens")
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno")
+        res.redirect("/admin/postagens")
+    })
+})
+
 
 module.exports = router
