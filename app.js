5018a796c42d557c3d4249d6172065db4b9387e6
joaosmatos <joaovsmatoss@gmail.com>
2025-12-28 01:03:08 -0300

    criando home page

--- app.js
+++ app.js
@@ -8,6 +8,8 @@
     const mongoose = require("mongoose")
     const session = require("express-session")
     const flash = require("connect-flash")
+    require("./models/postagem")
+    const Postagem = mongoose.model("postagens")
     //const mongoose = require("mongoose")
 // configurações
     // sessâo
@@ -46,7 +48,17 @@
 
 // rotas
     app.get('/', (req, res) => {
-        res.send('rota principal')
+        Postagem.find().populate("categoria").sort({data: "desc"}).then((postagens) => {
+            res.render('index', {postagens: postagens})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/404")
+        })
+        
+    })
+
+    app.get("/404", (req, res) => {
+        res.send('Erro 404!')
     })
 
     app.get('/posts', (req, res) => {
