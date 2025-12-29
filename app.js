abad137622fe95ab29acec5207db5b5ada569f6f
joaosmatos <joaovsmatoss@gmail.com>
2025-12-29 00:24:27 -0300

    pagina de postagem

--- app.js
+++ app.js
@@ -57,6 +57,20 @@
         
     })
 
+    app.get("/postagem/:slug", (req, res) => {
+        Postagem.findOne({slug: req.params.slug}).then((postagem) => {
+            if(postagem){
+                res.render("postagem/index", {postagem: postagem})
+            }else{
+                req.flash("error_msg", "Esta postagem não existe")
+                res.redirect("/")
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
+    })
+
     app.get("/404", (req, res) => {
         res.send('Erro 404!')
     })
