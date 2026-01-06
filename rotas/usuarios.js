968fdefb3beb2933435548b34fb881cc68236404
joaosmatos <joaovsmatoss@gmail.com>
2026-01-06 02:02:22 -0300

    rotas

--- rotas/usuarios.js
+++ rotas/usuarios.js
@@ -1,96 +1,121 @@
-const express = require("express");
-const router = express.Router();
-const mongoose = require("mongoose");
-require("../models/User");
-const User = mongoose.model("User");
-const bcrypt = require("bcryptjs");
-const passport = require("passport");
+const express = require("express")
+const router = express.Router()
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+const passport = require("passport")
 
-router.get("/register", (req, res) => {
-    res.render("users/register");
-});
+require("../models/Usuario")
+const Usuario = mongoose.model("usuarios")
 
-router.post("/register", (req, res) => {
-    let errors = [];
+router.get("/cadastro", (req, res) => {
+    res.render("usuarios/cadastro")
+})
 
-    if (!req.body.name) errors.push({ text: "Invalid name" });
+router.post("/cadastro", (req, res) =>{
 
-    if (!req.body.email) errors.push({ text: "Invalid email" });
+    var erros = []
 
-    if (!req.body.password) errors.push({ text: "Invalid password" });
-
-    if (req.body.password != req.body.password_confirmation)
-        errors.push({ text: "Passwords do not match" });
-
-    if (errors.length > 0) {
-        res.render("users/register", {
-            errors: errors,
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            password_confirmation: req.body.password_confirmation,
-        });
-    } else {
-        User.findOne({ email: req.body.email })
-            .then((user) => {
-                if (user) {
-                    req.flash("error_msg", "Email already registered");
-                    res.redirect("/users/register");
-                } else {
-                    const newUser = new User({
-                        name: req.body.name,
-                        email: req.body.email,
-                        password: req.body.password,
-                    });
-
-                    bcrypt.genSalt(10, (err, salt) => {
-                        bcrypt.hash(newUser.password, salt, (err, hash) => {
-                            if (err) {
-                                req.flash("error_msg", "Error registering");
-                                res.redirect("/");
-                            }
-                            newUser.password = hash;
-                            newUser
-                                .save()
-                                .then(() => {
-                                    req.flash(
-                                        "success_msg",
-                                        "You are registered",
-                                    );
-                                    res.redirect("/users/login");
-                                })
-                                .catch((err) => {
-                                    console.log(err);
-                                });
-                        });
-                    });
-                }
-            })
-            .catch((err) => {
-                req.flash("error_msg", "Something went wrong");
-                res.redirect("/users/register");
-            });
+    //Validação de dados do formulário
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido"})
     }
-});
+
+    if(!req.body.email || typeof req.body.email == undefined || req.body.email == null){
+        erros.push({texto: "E-mail inválido"})
+    }
+
+    if(!req.body.senha || typeof req.body.senha == undefined || req.body.senha == null){
+        erros.push({texto: "Senha inválida"})
+    }
+
+    //Verificar se a senha tem menos que 4 caracteres
+    if(req.body.senha.length < 4){
+        erros.push({texto: "Senha muito curta!"})
+    }
+
+    //Verificar se a senha repetida é diferente 
+    if(req.body.senha != req.body.senha2){
+        erros.push({texto: "As senhas são diferentes, tente novamente."})
+    }
+
+    if(erros.length > 0){
+
+        res.render("usuarios/registro", {erros: erros})
+
+    }else{
+        
+        Usuario.findOne({email: req.body.email}).then((usuario) => {    
+            if(usuario){
+                req.flash("error_msg", "Já existe uma conta com esse e-email no nosso sistema.")
+                res.redirect("/usuarios/registro")
+
+            }else{
+                 
+                const novoUsuario = new Usuario({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                })
+
+                //Gerar HASH de senha
+                //Salt -> um valor aleatório misturado com hash
+                bcrypt.genSalt(10, (erro, salt) => {
+                    bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+                        if(erro){
+                            req.flash("error_msg", "Houve um erro ao cadastrar usuário.")
+                            res.redirect("/")
+                        } 
+
+                        novoUsuario.senha = hash
+
+                        novoUsuario.save().then(() =>{
+
+                            req.flash("success_msg", "Usuário cadastrado com sucesso. ")
+                            res.redirect("/")
+
+                        }).catch((error) => {
+                            req.flash("error_msg", "Houve um erro ao cadastrar usuário.")
+                            res.redirect("/")
+                        })
+
+                        
+                    })
+                })   
+
+            }
+        }).catch((error) => {
+            req.flash("error_msg", "Houve um erro interno.")
+            res.redirect("/")
+        })
+
+    }
+})
 
 router.get("/login", (req, res) => {
-    res.render("users/login");
-});
+    res.render("usuarios/login")
+})
 
 router.post("/login", (req, res, next) => {
-    const { email, password } = req.body;
 
+    //Autenticar usuário
     passport.authenticate("local", {
-        successRedirect: "/",
-        failureRedirect: "/users/login",
-        failureFlash: true,
-    })(req, res, next);
-});
+        successRedirect: "/", // se autenticação ocorrer com sucesso
+        failureRedirect: "/usuarios/login", // se ocorrer alguma falha na autenticação
+        failureFlash: true
+    })(req, res, next)
 
+})
+
+//Deslogar do sistema
 router.get("/logout", (req, res) => {
-    req.logout();
-    req.flash("success_msg", "You are logged out");
-    res.redirect("/users/login");
+
+    req.logout()
+    req.flash("success_msg", "Deslogado com sucesso!")
+    res.redirect("/")
+
+
 });
 
-module.exports = router;
+
+
+module.exports = router
